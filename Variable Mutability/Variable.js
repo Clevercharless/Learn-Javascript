b82647c30ef8592db4519fc4f10ca4a5229e52a3
Clevercharless <charlesweslyinchrist@gmail.com>
2025-02-18 19:02:24 +0530

    Create Variable.js

---
+++ Variable Mutability/Variable.js
@@ -0,0 +1,8 @@
+const dozen = 12, halfDozen = 6, bakersDozen = 13;
+
+dozen = 13;
+
+var cookieCount = 5;
+let cookieCount = 5;
+
+// constant is something that doesn't change wheras var and let can be mutable
