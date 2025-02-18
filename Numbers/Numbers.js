baebaf870de1f7a5cd6ac6f3c55e5b9f6284a40a
Clevercharless <charlesweslyinchrist@gmail.com>
2025-02-18 18:51:15 +0530

    Create Numbers.js

---
+++ Numbers/Numbers.js
@@ -0,0 +1,37 @@
+12
+12.0
+12.82358972893527582
+-12
+Infinity
+-Infinity
+NaN
+
+var myNumber = 33;
+
+Math;
+Math.round(12.4984012840918);
+Math.round(12.92309820948209384);
+
+Math.random();
+Math.random();
+Math.random();
+Math.random();
+
+Math.
+Math.__proto__             Math.constructor           Math.hasOwnProperty        Math.isPrototypeOf         Math.propertyIsEnumerable  Math.toLocaleString        Math.toString
+Math.valueOf
+
+Math.E                     Math.LN10                  Math.LN2                   Math.LOG10E                Math.LOG2E                 Math.PI                    Math.SQRT1_2
+Math.SQRT2                 Math.abs                   Math.acos                  Math.acosh                 Math.asin                  Math.asinh                 Math.atan
+Math.atan2                 Math.atanh                 Math.cbrt                  Math.ceil                  Math.clz32                 Math.cos                   Math.cosh
+Math.exp                   Math.expm1                 Math.floor                 Math.fround                Math.hypot                 Math.imul                  Math.log
+Math.log10                 Math.log1p                 Math.log2                  Math.max                   Math.min                   Math.my                    Math.pow
+Math.random                Math.round                 Math.sign                  Math.sin                   Math.sinh                  Math.sqrt                  Math.tan
+Math.tanh                  Math.trunc
+
+
+
+
+my.__proto__             my.hasOwnProperty        my.isPrototypeOf         my.propertyIsEnumerable
+
+my.constructor           my.toExponential         my.toFixed               my.toLocaleString        my.toPrecision           my.toString              my.valueOf
