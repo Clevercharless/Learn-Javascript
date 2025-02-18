99f6251eee45ce82d9b9159ba6f2885db1034957
Clevercharless <charlesweslyinchrist@gmail.com>
2025-02-18 19:00:50 +0530

    Create Boolean.js

---
+++ Boolean Expressions/Boolean.js
@@ -0,0 +1,18 @@
+true;
+false;
+
+True;
+FALSE;
+tRuE;
+true;
+false;
+
+buttonHasBeenClicked = false;
+
+var myLocation = "Santa Barbara", myOtherLocation = "Los Angeles";
+
+myLocation === myOtherLocation;
+
+myOtherLocation = "Santa Barbara";
+
+myLocation === myOtherLocation;
