c0a97f235401c8b75c1ea687f7b070138307465f
Charless Binny <charlesweslyinchrist@gmail.com>
2025-03-03 22:57:29 +0530

    Create Basic.js

---
+++ Array/Basic.js
@@ -0,0 +1,15 @@
+var myArray = [];
+myArray;
+
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday"];
+daysOfTheWeek;
+
+var myList = [0, 1, 2, "string1", "string2", "string3", true, false];
+myList;
+
+var counties = ["Belknap", "Strafford", "Carroll", "Rockingham"];
+counties;
+
+var listOfStuff = [{ name: "value" }, [1, 2, 3], true, "nifty"];
+listOfStuff;
+listOfStuff.length;
